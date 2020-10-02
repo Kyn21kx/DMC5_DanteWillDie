f9cb98032debfd3edeb5f809f0370fbe3a8525bf
Nef <nefesmeralda@gmail.com>
2020-10-02 01:28:42 -0500

    Got base address right

--- ProcessMemoryReader.cs
+++ ProcessMemoryReader.cs
@@ -7,9 +7,13 @@ namespace DanteWillDie {
     public class ProcessMemoryReader {
         
         #region Variables
-        public Process ReadProcess { get; set; }
+        public Process ReadProcess { get; private set; }
         private IntPtr handle;
         #endregion
+
+        public ProcessMemoryReader (Process readProcess) {
+            ReadProcess = readProcess;
+        }
 
         public void OpenProcess () {
             uint access = (uint) GetAccesType(); 
@@ -26,6 +30,21 @@ namespace DanteWillDie {
             return ReaderApi.ProcessAccessType.PROCESS_QUERY_INFORMATION |
                 ReaderApi.ProcessAccessType.PROCESS_VM_READ | ReaderApi.ProcessAccessType.PROCESS_VM_WRITE |
                 ReaderApi.ProcessAccessType.PROCESS_VM_OPERATION;
+        }
+
+        public byte[] ReadMemory(IntPtr memoryAddress, uint bytesToRead, out int bytesRead) {
+            byte[] buffer = new byte[bytesToRead];
+            IntPtr pBytesRead = IntPtr.Zero;
+            ReaderApi.ReadProcessMemory(handle, memoryAddress, buffer, bytesToRead, out pBytesRead);
+            bytesRead = pBytesRead.ToInt32(); //This might be unnecessary
+            return buffer;
+        }
+
+        public void WriteMemory(IntPtr memoryAddress, byte[] buffer, out int bytesWritten) {
+            //Add error handling
+            IntPtr pBytesWritten = IntPtr.Zero;
+            ReaderApi.WriteProcessMemory(handle, memoryAddress, buffer, (uint)buffer.Length, out pBytesWritten);
+            bytesWritten = pBytesWritten.ToInt32();
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
 
 namespace DanteWillDie {
     class Program {
+
+        #region Variables
+        static ProcessMemoryReader memReader;
+        const uint MODULE_PTR = 0x07A98340;
+        const uint OFFSET_PTR = 10;
+        static uint baseAddress;
+        static int bytesOut = 0;
+        #endregion
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Initialize();
+        }
+
+        private static void Initialize () {
+            Process dmc5 = Process.GetProcessesByName("DevilMayCry5").ToList().FirstOrDefault();
+            if (dmc5 != null) {
+                memReader = new ProcessMemoryReader(dmc5);
+                memReader.OpenProcess();
+                IntPtr finalModule = (IntPtr)(MODULE_PTR + (ulong)dmc5.MainModule.BaseAddress);
+                baseAddress = BitConverter.ToUInt32(memReader.ReadMemory(finalModule, 4, out bytesOut));
+            }
+            else {
+                Exception up = new Exception("The game has not been started or found, please make sure the process is running properly under the name \"DevilMayCry5.exe\"!");
+                throw up;
+            }
         }
     }
 }
